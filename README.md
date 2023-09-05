6cc30dff812f93557e0f3b65424982707466fbef
crosleyzack <crosleyzack@gmail.com>
2023-09-05 09:47:35 -0400

    Basic meditation script

---
+++ README.md
@@ -0,0 +1,5 @@
+## Guided Stoic Meditation
+
+Guides the user through a stoic meditation on start up using `notify-send`. A simple but effective means of doing a morning meditation before starting the day.
+
+To install, simply download shell script and set to run on startup.
---
+++ meditation.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# First, reflect on the good things in life
+notify-send -u critical -t 0 'Stoic Meditation' 'Visualize the preferred indifferents in your life you are fortunate to have.'
+
+# Second, imagine losing those things
+notify-send -u critical -t 0 'Stoic Meditation' 'Visualize losing those preferred indifferents and recognize that they are indifferent.'
+
+# Third, recognize your ability to overcome challenges in your life
+notify-send -u critical -t 0 'Stoic Meditation' 'Visualize the dispreffered indifferents in your life and how you can overcome them with wisdom.'
+
+# Fourth, recognize the smallness of your problems by doing
+notify-send -u critical -t 0 'Stoic Meditation' 'Visualize the vastness of the universe and the small place of your problems within it.'
+
+# Finally, reflect on a daily quote
+notify-send -u critical -t 0 'Stoic Meditation' 'Reflect for a minute on todays desktop quote and how you can apply it in your life.'

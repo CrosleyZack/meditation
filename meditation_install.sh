1680f173767b58c6128f9d27141b730a9468b445
crosleyzack <crosleyzack@gmail.com>
2023-10-06 14:01:50 -0400

    Fix spelling error and add dependency

--- meditation_install.sh
+++ meditation_install.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
+sudo apt-get install -y libnotify-bin
+
 FILE_PATH=$(realpath $BASH_SOURCE)
 # echo "FILE_PATH = $FILE_PATH"
 DIR_PATH=$(dirname $FILE_PATH)
 # echo "DIR_PATH = $DIR_PATH"
 
 # Create startup.desktop
-DESKTOP_FILE="$DIR_PATH/mediation.desktop"
+DESKTOP_FILE="$DIR_PATH/meditation.desktop"
 rm -f $DESKTOP_FILE
 echo "[Desktop Entry]
 Type=Application
 Name=MediationScript
-Exec=$DIR_PATH/mediation.sh
+Exec=$DIR_PATH/meditation.sh
 OnlyShowIn=GNOME;" > $DESKTOP_FILE
 
 # link to generated startup script.

550b33d0837abddeeb5fe0cc3414ac307d5307ab
crosleyzack <zcrosley@censys.com>
2024-03-25 09:13:29 -0400

    Meditations selected randomly from a file

--- meditation.sh
+++ meditation.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-# First, reflect on the good things in life
-notify-send -u critical -t 0 'Stoic Meditation' 'Visualize the preferred indifferents in your life you are fortunate to have.'
+# Ensure a base directory was provided.
+FILE="${1:-mediations.json}"
+COUNT="${2:-4}"
 
-# Second, imagine losing those things
-notify-send -u critical -t 0 'Stoic Meditation' 'Visualize losing those preferred indifferents and recognize that they are indifferent.'
+# get json blob
+FILE_TEXT=$(<$FILE)
+JQ_SELECTOR=".[]"
+IFS=$'\n'
+THIS_JSON=($( echo $FILE_TEXT | jq -c "$JQ_SELECTOR" | shuf -n $COUNT ))
+for OUTPUT in ${THIS_JSON[@]}; do
+    # echo "$OUTPUT"
+    notify-send -u critical -t 0 'Meditation' "$OUTPUT"
+done
+notify-send -u critical -t 0 'Meditation' 'Reflect for a minute on todays desktop quote and how you can apply it in your life'
 
-# Third, recognize your ability to overcome challenges in your life
-notify-send -u critical -t 0 'Stoic Meditation' 'Visualize the dispreffered indifferents in your life and how you can overcome them with wisdom.'
-
-# Fourth, recognize the smallness of your problems by doing
-notify-send -u critical -t 0 'Stoic Meditation' 'Visualize the vastness of the universe and the small place of your problems within it.'
-
-# Finally, reflect on a daily quote
-notify-send -u critical -t 0 'Stoic Meditation' 'Reflect for a minute on todays desktop quote and how you can apply it in your life.'

43003442beb6e8f4966932e7b0ae143011752c90
crosleyzack <zcrosley@censys.com>
2024-03-25 13:11:14 -0400

    Fix cronjob

--- meditation_cron.sh
+++ meditation_cron.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# How often to run
-HOUR=$1
-if [ -z "$HOUR" ]
+# How often to run. default to every four hours
+HOUR="${1:-4}"
+# What minute to run at, generate randomly if not specified
+MIN=$2
+if [ -z "$MIN" ]
 then
-    HOUR=$((1 + $RANDOM % 5))
+    MIN=$(($RANDOM % 60))
 fi
-# TODO remove
-HOUR=01
 
 # Get executable to run
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
@@ -15,7 +15,7 @@ echo "mediatation_cron SCRIPT_DIR = $SCRIPT_DIR"
 logger "meditation_cron SCRIPT_DIR = $SCRIPT_DIR"
 EXE="$SCRIPT_DIR/meditation.sh"
 
-# run meditation script
+# run meditation script _now_
 bash $EXE
 
 # Add new cronjob if doesn't already exist
@@ -24,11 +24,9 @@ crontab -l > crontab_new
 found=$( grep $EXE crontab_new )
 if [ -z "$found" ]
 then
-    echo "meditation_cron adding cron to crontab"
-    logger "meditation_cron adding cron to crontab"
-    # TODO swap
-    # echo "00 $HOUR * * * $EXE" >> crontab_new
-    echo "$HOUR 0 * * * $EXE" >> crontab_new
+    echo "meditation_cron adding cron to crontab every $HOUR at $MIN minutes"
+    logger "meditation_cron adding cron to crontab every $HOUR at $MIN minutes"
+    echo "$MIN */$HOUR * * * $EXE" >> crontab_new
 fi
 crontab crontab_new
 rm crontab_new
